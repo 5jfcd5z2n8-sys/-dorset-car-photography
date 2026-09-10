5bf8ee105a2aa6a5eeb13a2cd76378a94cf9ba46
5jfcd5z2n8-sys <5jfcd5z2n8@privaterelay.appleid.com>
2026-09-10 17:30:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+// Small enhancement: update the footer year automatically.
+document.querySelector('footer').innerHTML =
+  '© ' + new Date().getFullYear() + ' Dorset Car Photography · Capturing Dorset\'s roads, one car at a time.';
